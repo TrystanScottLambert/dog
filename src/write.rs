7737b7408ec319b96e723743a93f718902537eaf
TrystanScottLambert <trystanscottlambert@gmail.com>
2026-07-10 16:38:39 +0800

    making lazyframe clone instead so we can pass reference in write_parquet

--- src/write.rs
+++ src/write.rs
@@ -2,9 +2,9 @@ use anyhow::Result;
 use polars::prelude::*;
 use std::{fs::File, path::PathBuf};
 
-pub fn write_parquet(lazy_frame: LazyFrame, output_path: &PathBuf) -> Result<()> {
+pub fn write_parquet(lazy_frame: &LazyFrame, output_path: &PathBuf) -> Result<()> {
     let file = File::create(output_path)?;
-    let mut df = lazy_frame.collect().unwrap();
+    let mut df = lazy_frame.clone().collect().unwrap();
 
     ParquetWriter::new(file).finish(&mut df)?;
 
